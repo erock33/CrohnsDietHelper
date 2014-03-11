e0a50edee72e5bcd639373ab1316d9445eeae0f5
Eric Hobbs <eric.hobbs33@gmail.com>
2014-03-11 16:40:34 -0700

    Added some exception handling to main() method

--- src/CrohnsDietHelperMain.java
+++ src/CrohnsDietHelperMain.java
@@ -1,9 +1,22 @@
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 
+import crohnsdiethelper.controller.Workflow;
+import de.derivo.sparqldlapi.exceptions.QueryEngineException;
+import de.derivo.sparqldlapi.exceptions.QueryParserException;
+
+/*
+ * This class just holds the main method and
+ * is responsible for performing the bare 
+ * minimum to get the program up and running.
+ * 
+ */
 public class CrohnsDietHelperMain {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+    
+    public static void main(String[] args) throws OWLOntologyCreationException, QueryParserException, QueryEngineException {
+        // TODO Auto-generated method stub
+        Workflow workflow = new Workflow();
+        workflow.configure();
+        workflow.start();
+    }
 
 }
