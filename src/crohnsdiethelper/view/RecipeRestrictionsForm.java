645ba0a8dac997c7a5a347758b16b7863c312815
Eric Hobbs <eric.hobbs33@gmail.com>
2014-03-11 21:06:39 -0700

    Added ingredient list and color combo box to recipe restrictions form.

--- src/crohnsdiethelper/view/RecipeRestrictionsForm.java
+++ src/crohnsdiethelper/view/RecipeRestrictionsForm.java
@@ -1,5 +1,42 @@
 package crohnsdiethelper.view;
 
-public class RecipeRestrictionsForm {
+import java.util.ArrayList;
 
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
+import javax.swing.ListSelectionModel;
+
+import crohnsdiethelper.model.Ontology;
+import de.derivo.sparqldlapi.exceptions.QueryEngineException;
+import de.derivo.sparqldlapi.exceptions.QueryParserException;
+
+public class RecipeRestrictionsForm {
+    JPanel panel;
+    
+    public JPanel buildForm(Ontology ontology) throws QueryParserException, QueryEngineException {
+        this.panel = new JPanel();
+        
+        ArrayList<String> foods = ontology.getLeafLabels();
+
+        String[] arr = foods.toArray(new String[ foods.size() ]);
+        JList mylist = new JList(arr);
+        mylist.setSelectionMode( ListSelectionModel.MULTIPLE_INTERVAL_SELECTION );
+        
+        JScrollPane spane = new JScrollPane(mylist);
+//        JScrollBar mylistscroll = new JScrollBar( JScrollBar.VERTICAL );
+        
+        this.panel.add(spane);
+        
+        String[] colors = {"Green", "Orange", "Purple", "Red"};
+        JComboBox juicecolors = new JComboBox(colors);
+        this.panel.add(juicecolors);
+        
+        
+        
+        return this.panel;
+    }
 }
